bc463a4db17bf528cd34bb925c7f1a369e2f2a1c
Jon Laos <jlaos526@gmail.com>
2026-05-02 11:15:24 -0400

    chore: initial Chiaro monorepo setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.argv[2] || 3000;
+const ROOT = __dirname;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'application/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(ROOT, req.url === '/' ? '/Home_Screen/code.html' : req.url);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`Chiaro running on http://localhost:${PORT}`));
