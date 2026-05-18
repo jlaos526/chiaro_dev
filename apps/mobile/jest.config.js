72323abe1ec78f91737b1b87f87a19f05e2c5932
Jon Laos <jlaos526@gmail.com>
2026-05-18 19:58:29 -0400

    feat(mobile): @sentry/react-native + lib/sentry.ts (init + scrubber + ErrorBoundary re-export)

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testMatch: ['<rootDir>/test/**/*.test.{ts,tsx}'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|@sentry/.*|native-base|react-native-svg)',
   ],
 }
